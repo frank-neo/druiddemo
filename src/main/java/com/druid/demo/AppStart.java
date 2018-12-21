90f2c2d85abc27d943a6fee84d119f2c64312c43
f53_100004489579_FrankLee <f53_100004489579_FrankLee@git.cloud.tencent.com>
2018-12-21 16:20:40 +0800

    整合了druid

--- src/main/java/com/druid/demo/AppStart.java
+++ src/main/java/com/druid/demo/AppStart.java
@@ -3,9 +3,11 @@ package com.druid.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.druid.demo.mapper")
+@ServletComponentScan//这行是为了避免扫描不到Druid的Servlet
 public class AppStart {
     public static void main (String[] args){
 
